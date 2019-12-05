97dd3c914bf5930c02f5ebfbd3acab6f03c8f618
Bruno Coimbra <coimbra@fnal.gov>
2019-12-05 15:47:15 -0600

    Implement write and _interactive parameters

--- pychirp.py
+++ pychirp.py
@@ -12,51 +12,54 @@ from inspect import getargspec
 # Functions take arguments from the command line when True
 interactive = False
 
-def _interactive(func):
+def _interactive(custom={}):
     """Makes the function callable from a console.
     """
-    def wrapper(*args, **kwargs):
-        if interactive:
-            # Parser initialization
-            parser = argparse.ArgumentParser()
-            
-            # Define command usage
-            parser.prog = "%s %s" % (parser.prog, func.__name__)
-            
-            # Define command help based on an available docstring
-            if func.__doc__:
-                parser.description = re.split(r"\n\s*\n", func.__doc__)[0]
-            
-            # Retrieve function signature to build arguments
-            args, _, _, defaults = getargspec(func)
-            if defaults:
-                defaults = dict(zip(args[-len(defaults):], defaults))
-            else:
-                defaults = {}
-
-            # Add arguments to the parser and tries to extract help from an available docstring
-            for arg in args:
-                arghelp = None
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if interactive:
+                # Parser initialization
+                parser = argparse.ArgumentParser()
+                
+                # Define command usage
+                parser.prog = "%s %s" % (parser.prog, func.__name__)
+                
+                # Define command help based on an available docstring
                 if func.__doc__:
-                    argdoc = re.findall(r"%s\s\(.*\)\:\s(.*)" % arg, func.__doc__)
-                    if argdoc:
-                        arghelp = re.sub(r"\sdefaults\sto\s.*", "", argdoc[0].lower()).strip(".")
-                if arg not in defaults:
-                    parser.add_argument(arg, help=arghelp)
+                    parser.description = re.split(r"\n\s*\n", func.__doc__)[0]
+                
+                # Retrieve function signature to build arguments
+                args, _, _, defaults = getargspec(func)
+                if defaults:
+                    defaults = dict(zip(args[-len(defaults):], defaults))
                 else:
-                    argnargs = None
-                    if isinstance(defaults[arg], tuple):
-                        argnargs = len(defaults[arg])
-                    parser.add_argument("-" + arg, help=arghelp, nargs=argnargs)
+                    defaults = {}
 
-            # Parse system args
-            parsed_args = parser.parse_args(sys.argv[2:])
+                # Add arguments to the parser and tries to extract help from an available docstring
+                for arg in args:
+                    arghelp = None
+                    if func.__doc__:
+                        argdoc = re.findall(r"%s\s\(.*\)\:\s(.*)" % arg, func.__doc__)
+                        if argdoc:
+                            arghelp = re.sub(r"\sdefaults\sto\s.*", "", argdoc[0].lower()).strip(".")
+                    argname = arg
+                    argoptions = {"help": arghelp}
+                    if arg in defaults:
+                        argname = "-" + arg
+                    if arg in custom:
+                        argoptions.update(custom[arg])
+                    parser.add_argument(argname, **argoptions)
 
-            return func(**vars(parsed_args))
-        return func(*args, **kwargs)
-    return wrapper
+                # Parse system args
+                parsed_args = vars(parser.parse_args(sys.argv[2:]))
+                parsed_args = dict(filter(lambda item: item[1] or item[0] not in defaults, parsed_args.items()))
+                
+                return func(**parsed_args)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
-@_interactive
+@_interactive()
 def fetch(remote_file, local_file):
     """Copy the remote_file from the submit machine to the execute machine, naming it local_file.
     
@@ -71,8 +74,8 @@ def fetch(remote_file, local_file):
     with htchirp.HTChirp() as chirp:
         return chirp.fetch(remote_file, local_file)
 
-@_interactive
-def put(remote_file, local_file, mode=None, perm=None):
+@_interactive()
+def put(remote_file, local_file, mode="wct", perm=None):
     """Copy the local_file from the execute machine to the submit machine, naming it remote_file.
        The optional perm argument describes the file access permissions in a Unix format.
        The optional mode argument is one or more of the following characters describing the remote_file file:
@@ -82,16 +85,13 @@ def put(remote_file, local_file, mode=None, perm=None):
     Args:
         remote_file (string, optional): File on submit machine.
         local_file (string, optional): File on execute machine.
-        mode (string, optional): File open modes (one or more of 'watcx'). Defaults to None.
+        mode (string, optional): File open modes (one or more of 'watcx'). Defaults to 'wct'.
             w, open for writing;
             a, force all writes to append;
             t, truncate before use;
             c, create the file, if it does not exist;
             x, fail if c is given and the file already exists. Defaults to None.
         perm (string, optional): Describes the file access permissions in a Unix format. Defaults to None.
-    
-    Returns:
-        integer: Size of written file
     """
     opt_params = {}
     if mode:
@@ -105,9 +105,9 @@ def put(remote_file, local_file, mode=None, perm=None):
         opt_params["mode"] = perm
 
     with htchirp.HTChirp() as chirp:
-        return chirp.put(remote_file, local_file, mode, perm)
+        chirp.put(remote_file, local_file, mode, perm)
 
-@_interactive
+@_interactive()
 def remove(remote_file):
     """Remove the remote_file file from the submit machine.
     
@@ -118,7 +118,7 @@ def remove(remote_file):
     with htchirp.HTChirp() as chirp:
         chirp.remove(remote_file)
 
-@_interactive
+@_interactive()
 def get_job_attr(job_attribute):
     """Prints the named job ClassAd attribute to standard output.
     
@@ -132,7 +132,7 @@ def get_job_attr(job_attribute):
     with htchirp.HTChirp() as chirp:
         return chirp.get_job_attr(job_attribute)
 
-@_interactive
+@_interactive()
 def set_job_attr(job_attribute, attribute_value):
     """Sets the named job ClassAd attribute with the given attribute value.
     
@@ -144,7 +144,7 @@ def set_job_attr(job_attribute, attribute_value):
     with htchirp.HTChirp() as chirp:
         chirp.set_job_attr(job_attribute, attribute_value)
 
-@_interactive
+@_interactive()
 def get_job_attr_delayed(job_attribute):
     """Prints the named job ClassAd attribute to standard output, potentially reading the cached value from a recent set_job_attr_delayed.
     
@@ -158,7 +158,7 @@ def get_job_attr_delayed(job_attribute):
     with htchirp.HTChirp() as chirp:
         return chirp.get_job_attr_delayed(job_attribute)
 
-@_interactive
+@_interactive()
 def set_job_attr_delayed(job_attribute, attribute_value):
     """Sets the named job ClassAd attribute with the given attribute value, but does not immediately
        synchronize the value with the submit side. It can take 15 minutes before the synchronization occurs.
@@ -174,7 +174,7 @@ def set_job_attr_delayed(job_attribute, attribute_value):
     with htchirp.HTChirp() as chirp:
         chirp.set_job_attr_delayed(job_attribute, attribute_value)
 
-@_interactive
+@_interactive()
 def ulog(text):
     """Appends Message to the job event log.
     
@@ -185,10 +185,10 @@ def ulog(text):
     with htchirp.HTChirp() as chirp:
         chirp.ulog(text)
 
-@_interactive
+@_interactive({"stride":{"nargs": 2, "metavar": ("LENGTH", "SKIP")}})
 def read(remote_file, length, offset=None, stride=(None, None)):
     """Read length bytes from remote_file. Optionally, implement a stride by starting the read at offset
-       and reading stride[0](length) bytes with a stride of stride[1](skip) bytes.
+       and reading stride(length) bytes with a stride of stride(skip) bytes.
     
     Args:
         remote_file (string): File on the submit machine.
@@ -202,6 +202,26 @@ def read(remote_file, length, offset=None, stride=(None, None)):
 
     with htchirp.HTChirp() as chirp:
         return chirp.read(remote_file, length, offset, stride[0], stride[1])
+
+@_interactive({"length": {"nargs": "?"}, "stride":{"nargs": 2, "metavar": ("LENGTH", "SKIP")}})
+def write(remote_file, local_file, length, offset=None, stride=(None, None)):
+    """Write the contents of local_file to remote_file. Optionally, start writing to the remote file at offset
+       and write stride(length) bytes with a stride of stride(skip) bytes. If the optional length follows
+       local_file, then the write will halt after length input bytes have been written. Otherwise, the entire
+       contents of local_file will be written.
+    
+    Args:
+        remote_file (string): File on the submit machine.
+        local_file (string): File on execute machine.
+        length (int): Number of bytes to write.
+        offset (integer, optional): Number of bytes to offset from beginning of file. Defaults to None.
+        stride (tuple, optional): Number of bytes to read followed by number of bytes to skip per stride. Defaults to (None, None).
+    """
+
+    data = open(local_file).read()
+
+    with htchirp.HTChirp() as chirp:
+        chirp.write(data, remote_file, length=length, offset=offset, stride_length=stride[0], stride_skip=stride[1])
 
 if __name__ == "__main__":
     # Help text
